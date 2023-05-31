501584de2fd3d58947eda3526510114fc9f91653
Nef Maxime <maximenef1@gmail.com>
2023-05-31 14:54:31 +0200

    change la langue par defaut

--- taximan-main/i18n.tsx
+++ taximan-main/i18n.tsx
@@ -1,33 +1,28 @@
-import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: 
-    {
-      en: 
-      {
-        translation: require('public/locales/en/translation.json')
+    resources: {
+      en: {
+        translation: require("public/locales/en/translation.json"),
       },
-      
-      fr: 
-      {
-        translation: require('public/locales/fr/translation.json')
+
+      fr: {
+        translation: require("public/locales/fr/translation.json"),
       },
-      
-      nl: 
-      {
-        translation: require('public/locales/nl/translation.json')
-      }
+
+      nl: {
+        translation: require("public/locales/nl/translation.json"),
+      },
     },
-    fallbackLng: 'fr',
-    interpolation: 
-    {
-      escapeValue: false
-    }
+    fallbackLng: "nl",
+    interpolation: {
+      escapeValue: false,
+    },
   });
 
 export default i18n;
