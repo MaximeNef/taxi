41e00aca6feeba13f88124b7d3e5fd100550e158
Nef Maxime <maximenef1@gmail.com>
2023-06-01 10:10:49 +0200

    change numero

--- taximan-main/src/components/buttonWhite.tsx
+++ taximan-main/src/components/buttonWhite.tsx
@@ -1,27 +1,37 @@
 import Image from "next/image";
 import { useTranslation } from "react-i18next";
 
-interface Props 
-{
-   className? : string
-   background : boolean
+interface Props {
+  className?: string;
+  background: boolean;
 }
 
-const ButtonWhite = ({className, background}: Props) => 
-{
-    const { t } = useTranslation();
+const ButtonWhite = ({ className, background }: Props) => {
+  const { t } = useTranslation();
 
+  return (
+    <button
+      className={`${className} ${
+        background ? "bg-white" : "bg-none"
+      } text-theme-yellow hover:shadow-button hover:scale-105 transition duration-400 ease-in-out rounded-[40px] py-[10px] px-[24px]    `}
+    >
+      <div className='flex flex-row'>
+        <a
+          href='tel:0032/489.59.36.82'
+          className='font-inter text-[19px] font-[400] mr-[10px] leading-20 tracking-wider text-center'
+        >
+          {t("lastSection.button")}
+        </a>
+        <Image
+          src='assets/logos/phoneYellow.svg'
+          alt='logo telephone'
+          className='.phone-icon'
+          width={20}
+          height={20}
+        />
+      </div>
+    </button>
+  );
+};
 
-    return (
-        <button className={`${className} ${background ? "bg-white" : "bg-none"} text-theme-yellow hover:shadow-button hover:scale-105 transition duration-400 ease-in-out rounded-[40px] py-[10px] px-[24px]    `} >
-            <div className="flex flex-row">
-                <a href="tel:0472 827 372" className="font-inter text-[19px] font-[400] mr-[10px] leading-20 tracking-wider text-center">{t("lastSection.button")}</a>
-                 <Image src="assets/logos/phoneYellow.svg" alt="logo telephone" className=".phone-icon" width={20} height={20} />
-            </div>
-        </button>
-    )
-}
-
-export default ButtonWhite 
-
-  
+export default ButtonWhite;
