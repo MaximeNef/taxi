41293a62e3da3e1b251aa4e83ae2066ba46c0d2e
Nef Maxime <maximenef1@gmail.com>
2023-06-05 10:49:23 +0200

    moove script from index to APp page

--- taximan-main/src/pages/_app.tsx
+++ taximan-main/src/pages/_app.tsx
@@ -1,24 +1,39 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { appWithTranslation } from 'next-i18next';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
-import { I18nextProvider } from 'react-i18next';
-import i18n from '../../i18n';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { appWithTranslation } from "next-i18next";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../../i18n";
+import Head from "next/head";
+import Script from "next/script";
 
-const App = ({ Component, pageProps }: AppProps) => 
-{
+const App = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
-  AOS.init();
+    AOS.init();
   }, []);
 
   return (
-  <I18nextProvider i18n={i18n}>
-  <Component {...pageProps} />
-  </I18nextProvider>
+    <>
+      <Head>
+        {/* UXWIZZ script update script */}
+        <Script id='uxwizz'>
+          UST_CT = []; UST = "
+          {"s: Date.now(), addTag: function(tag) {UST_CT.push(tag)} "}
+          ";UST.addEvent = UST.addTag;
+        </Script>
+        <Script
+          src='https://stats.listri.digital/server/ust.min.js?v=4.5.0'
+          async
+        ></Script>
+        {/* FIN UXWIZZ script  */}
+      </Head>
+      <I18nextProvider i18n={i18n}>
+        <Component {...pageProps} />
+      </I18nextProvider>{" "}
+    </>
   );
 };
 
 export default appWithTranslation(App);
-
--- taximan-main/src/pages/index.tsx
+++ taximan-main/src/pages/index.tsx
@@ -7,7 +7,6 @@ import Hero from "@/components/Hero";
 import WelcomeSection from "@/components/WelcomeSection";
 import ThirdSection from "@/components/ThirdSection";
 import LastSection from "@/components/LastSection";
-import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,17 +22,6 @@ export default function Home() {
           content='qq4msw35AbpTxunoNYt6pBE822Q0Qi2rD-S2Y7w7M9w'
         />
         <link rel='icon' href='/favicon.ico' />
-        {/* UXWIZZ script update script */}
-        <Script id='uxwizz'>
-          UST_CT = []; UST = "
-          {"s: Date.now(), addTag: function(tag) {UST_CT.push(tag)} "}
-          ";UST.addEvent = UST.addTag;
-        </Script>
-        <Script
-          src='https://stats.listri.digital/server/ust.min.js?v=4.5.0'
-          async
-        ></Script>
-        {/* FIN UXWIZZ script  */}
         {/* <link href="/dist/output.css" rel="stylesheet"></link> */}
       </Head>
 
