25f23b32aa84d7b8ab04f1c4432051e5587d4b28
Nef Maxime <maximenef1@gmail.com>
2024-02-01 17:24:19 +0100

    update taxi v2

---
+++ taximan-main/.next/static/webpack/pages/_app.6520d3a7412cf3a5.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/_app",{
+
+/***/ "./public/locales/fr/translation.json":
+/*!********************************************!*\
+  !*** ./public/locales/fr/translation.json ***!
+  \********************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"hero":{"title":"La compagnie de taxis la plus fiable de Knokke"},"button":{"buttonText":"Appeler"},"welcomeSection":{"h3":"CE QUE L\'ON PROPOSE","h2":"Bienvenue chez Uilenspiegel","p":"Nous avons créé notre taxi pour vous aider à trouver le taxi le plus fiable et de la meilleure qualité, à tout moment et en tout lieu.","subsectionsData":[{"h4Text":"Adresse Pickup","pText1":"Nous venons toujours","pText2":"chercher nos clients à l\'heure,","pText3":"24 heures sur 24, 7 jours sur 7."},{"h4Text":"Longue distance","pText1":"Nous vous offrons un service","pText2":"de taxi longue distance vers","pText3":"n\'importe quel endroit."},{"h4Text":"Trajet sur mesure","pText1":"Nous mettons notre expertise","pText2":"à votre service pour des trajets","pText3":"sur mesure."}]},"thirdSection":{"h3":"CARACTERISTIQUES PRINCIPALES","h2":"Notre véhicule","secondSubsectionsData":[{"h4Text":"Prise en charge","pText":"rapide","srcImg":"assets/logos/timer.svg"},{"h4Text":"Course rapide","pText":"itinéraire sur mesure","srcImg":"assets/logos/speedometer.svg"},{"h4Text":"Sans frais","pText":"supplémentaire","srcImg":"assets/logos/lock.svg"}]},"lastSection":{"h3":"NOS POINTS FORTS","h2":"À propos","lastSubsectionsData":[{"srcImg":"assets/logos/watch.svg","h4Text":"Horaire","pText1":"Nous sommes à votre service 24/24h 7/7j.","pText2":"","pText3":""},{"srcImg":"assets/logos/world.svg","h4Text":"Langues","pText1":"Nous parlons votre langue : Français,","pText2":"Néerlandais, Anglais, Allemand","pText3":""},{"srcImg":"assets/logos/contact.svg","h4Text":"Contact","pText1":"Vous pouvez nous joindre par","pText2":"Téléphone : 0032/489.59.36.82","pText3":"Mail : streettaxiknokke@gmail.com"}],"button":"Appeler"},"footer":{"pContact":"Contactez-nous","pPhone":"0032/489.59.36.82"}}');
+
+/***/ })
+
+});
