586f3f113283701eb770e208d47a123272e32032
Nef Maxime <maximenef1@gmail.com>
2024-02-19 22:58:18 +0100

    version 2 du site web

---
+++ taximan-main/.next/static/webpack/pages/_app.cd7732bd94df3dba.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/_app",{
+
+/***/ "./public/locales/en/translation.json":
+/*!********************************************!*\
+  !*** ./public/locales/en/translation.json ***!
+  \********************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"hero":{"title":"The most reliable taxi company in Knokke"},"button":{"buttonText":"Call us"},"welcomeSection":{"h3":"WHAT WE OFFER","h2":"Welcome to Street Taxi Knokke","p":"We have created our taxi to help you find the most reliable and best quality taxi, anytime, anywhere.","subsectionsData":[{"h4Text":"Address Pickup","pText1":"We always pick up","pText2":"our customers on time,","pText3":"24 hours a day, 7 days a week."},{"h4Text":"Long distance","pText1":"We offer you a","pText2":"long distance taxi service","pText3":"to any location."},{"h4Text":"Tailor-made journey","pText1":"We put our expertise","pText2":"at your service for","pText3":"tailor-made journeys."}]},"thirdSection":{"h3":"MAIN CHARACTERISTICS","h2":"Our vehicle ","secondSubsectionsData":[{"h4Text":"taken care of","pText":"fast","srcImg":"assets/logos/timer.svg"},{"h4Text":"fast travel","pText":"tailor-made itinerary","srcImg":"assets/logos/speedometer.svg"},{"h4Text":"no extra","pText":"charge","srcImg":"assets/logos/lock.svg"}]},"lastSection":{"h3":"OUR STRENGTHS","h2":"About us","lastSubsectionsData":[{"srcImg":"assets/logos/watch.svg","h4Text":"Schedule","pText1":"We are available 24/7.","pText2":"","pText3":""},{"srcImg":"assets/logos/world.svg","h4Text":"Languages","pText1":"We speak your language: French,","pText2":"Dutch, English, German","pText3":""},{"srcImg":"assets/logos/contact.svg","h4Text":"Contact","pText1":"You can reach us by","pText2":"Phone: 0032/489.59.36.82","pText3":"Email: gabymilles@yahoo.com"}],"button":"Call us"},"footer":{"pContact":"Contact us","pPhone":"0032/489.59.36.82"}}');
+
+/***/ })
+
+});
