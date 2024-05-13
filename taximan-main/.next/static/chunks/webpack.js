5989ac5aa0cadd5bb931e1805b622a5b39914671
Nef Maxime <maximenef1@gmail.com>
2024-05-13 18:17:13 +0200

    Version finale du site

--- taximan-main/.next/static/chunks/webpack.js
+++ taximan-main/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc84d1788126f6e8"; }
+/******/ 		__webpack_require__.h = function() { return "32a2e1024a511480"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ taximan-main/.next/static/webpack/pages/_app.e2628314810277d0.hot-update.js
@@ -0,0 +1,42 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/_app",{
+
+/***/ "./public/locales/en/translation.json":
+/*!********************************************!*\
+  !*** ./public/locales/en/translation.json ***!
+  \********************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"hero":{"title":"The most reliable taxi company in Knokke"},"button":{"buttonText":"Call us"},"welcomeSection":{"h3":"WHAT WE OFFER","h2":"Welcome to Uilenspiegel","p":"We have created our taxi to help you find the most reliable and best quality taxi, anytime, anywhere.","subsectionsData":[{"h4Text":"Address Pickup","pText1":"We always pick up","pText2":"our customers on time,","pText3":"24 hours a day, 7 days a week."},{"h4Text":"Long distance","pText1":"We offer you a","pText2":"long distance taxi service","pText3":"to any location."},{"h4Text":"Tailor-made journey","pText1":"We put our expertise","pText2":"at your service for","pText3":"tailor-made journeys."}]},"thirdSection":{"h3":"MAIN CHARACTERISTICS","h2":"Our vehicle ","secondSubsectionsData":[{"h4Text":"taken care of","pText":"fast","srcImg":"assets/logos/timer.svg"},{"h4Text":"fast travel","pText":"tailor-made itinerary","srcImg":"assets/logos/speedometer.svg"},{"h4Text":"no extra","pText":"charge","srcImg":"assets/logos/lock.svg"}]},"lastSection":{"h3":"OUR STRENGTHS","h2":"About us","lastSubsectionsData":[{"srcImg":"assets/logos/watch.svg","h4Text":"Schedule","pText1":"We are available 24/7.","pText2":"","pText3":""},{"srcImg":"assets/logos/world.svg","h4Text":"Languages","pText1":"We speak your language: French,","pText2":"Dutch, English, German","pText3":""},{"srcImg":"assets/logos/contact.svg","h4Text":"Contact","pText1":"You can reach us by","pText2":"Phone: 0032/489.59.36.82","pText3":"Email: streettaxiknokke@gmail.com"}],"button":"Call us"},"footer":{"pContact":"Contact us","pPhone":"0032/489.59.36.82"}}');
+
+/***/ }),
+
+/***/ "./public/locales/fr/translation.json":
+/*!********************************************!*\
+  !*** ./public/locales/fr/translation.json ***!
+  \********************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"hero":{"title":"La compagnie de taxis la plus fiable de Knokke"},"button":{"buttonText":"Appeler"},"welcomeSection":{"h3":"CE QUE L\'ON PROPOSE","h2":"Bienvenue chez Uilenspiegel","p":"Nous avons créé notre taxi pour vous aider à trouver le taxi le plus fiable et de la meilleure qualité, à tout moment et en tout lieu.","subsectionsData":[{"h4Text":"Adresse Pickup","pText1":"Nous venons toujours","pText2":"chercher nos clients à l\'heure,","pText3":"24 heures sur 24, 7 jours sur 7."},{"h4Text":"Longue distance","pText1":"Nous vous offrons un service","pText2":"de taxi longue distance vers","pText3":"n\'importe quel endroit."},{"h4Text":"Trajet sur mesure","pText1":"Nous mettons notre expertise","pText2":"à votre service pour des trajets","pText3":"sur mesure."}]},"thirdSection":{"h3":"CARACTERISTIQUES PRINCIPALES","h2":"Notre véhicule","secondSubsectionsData":[{"h4Text":"Prise en charge","pText":"rapide","srcImg":"assets/logos/timer.svg"},{"h4Text":"Course rapide","pText":"itinéraire sur mesure","srcImg":"assets/logos/speedometer.svg"},{"h4Text":"Sans frais","pText":"supplémentaire","srcImg":"assets/logos/lock.svg"}]},"lastSection":{"h3":"NOS POINTS FORTS","h2":"À propos","lastSubsectionsData":[{"srcImg":"assets/logos/watch.svg","h4Text":"Horaire","pText1":"Nous sommes à votre service 24/24h 7/7j.","pText2":"","pText3":""},{"srcImg":"assets/logos/world.svg","h4Text":"Langues","pText1":"Nous parlons votre langue : Français,","pText2":"Néerlandais, Anglais, Allemand","pText3":""},{"srcImg":"assets/logos/contact.svg","h4Text":"Contact","pText1":"Vous pouvez nous joindre par","pText2":"Téléphone : 0032/489.59.36.82","pText3":"Mail : streettaxiknokke@gmail.com"}],"button":"Appeler"},"footer":{"pContact":"Contactez-nous","pPhone":"0032/489.59.36.82"}}');
+
+/***/ }),
+
+/***/ "./public/locales/nl/translation.json":
+/*!********************************************!*\
+  !*** ./public/locales/nl/translation.json ***!
+  \********************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"hero":{"title":"Het betrouwbaarste taxibedrijf in Knokke"},"button":{"buttonText":"Bellen"},"welcomeSection":{"h3":"WAT WE VOORSTELLEN","h2":"Welkom bij Uilenspiegel","p":"Wij hebben onze taxi gemaakt om u te helpen de meest betrouwbare en beste kwaliteit taxi te vinden, altijd en overal.","subsectionsData":[{"h4Text":"Adres Pickup","pText1":"Wij halen ","pText2":"onze klanten altijd op tijd op","pText3":"24 uur per dag, 7 dagen per week."},{"h4Text":"Lange afstand","pText1":"Wij bieden","pText2":"een lange afstand","pText3":"taxi service naar elke locatie."},{"h4Text":"Traject op maat","pText1":"Wij stellen onze expertise","pText2":"tot uw dienst","pText3":"voor trajecten op maat."}]},"thirdSection":{"h3":"BELANGRIJKSTE KENMERKEN","h2":"Ons voertuig","secondSubsectionsData":[{"h4Text":"snelle","pText":"afhandeling","srcImg":"assets/logos/timer.svg"},{"h4Text":"Snelle rit","pText":"route op maat","srcImg":"assets/logos/speedometer.svg"},{"h4Text":"Geen extra","pText":"kosten","srcImg":"assets/logos/lock.svg"}]},"lastSection":{"h3":"ONZE STERKTES","h2":"Over ons","lastSubsectionsData":[{"srcImg":"assets/logos/watch.svg","h4Text":"Openingstijden","pText1":"Wij staan 24/7 tot uw dienst.","pText2":"","pText3":""},{"srcImg":"assets/logos/world.svg","h4Text":"Talen","pText1":"Wij spreken uw taal: Frans, Nederlands, ","pText2":"Engels, Duits","pText3":""},{"srcImg":"assets/logos/contact.svg","h4Text":"Contact","pText1":"U kunt ons bereiken via","pText2":"Telefoon: 0032/489.59.36.82","pText3":"Mail: streettaxiknokke@gmail.com"}],"button":"Bellen"},"footer":{"pContact":"Contacteer ons","pPhone":"0032/489.59.36.82"}}');
+
+/***/ })
+
+});
